28c1a5b3263d6ef12fa7177bc7eb8ed7cf5280ce
StefanTeji10 <stefan.stojacic@gmail.com>
2021-10-14 15:22:52 +0200

    Komunikacija putem ESP_NOW uspostavljena

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,63 @@
 #include <Arduino.h>
 #include <ADS1115_WE.h>
 #include<Wire.h>
+#include <esp_now.h>
+#include <WiFi.h>
 
 #define I2C_ADDRESS 0x48
+
+
+typedef struct struct_receive_message{
+  char flag;
+
+}struct_receive_message;
+
+char chReceiveFlag;
+
+typedef struct struct_send_message{
+  float accumulatedValue100times;
+  int counter;
+
+}struct_send_message;
+
+struct_send_message message2send;
+struct_receive_message incomingMessage;
+
+
+
+String success;
+
+
+
+
+
+// MAC address of our receiving device
+uint8_t broadcastAddress[] = {0xE0, 0xE2, 0xE6, 0x4F, 0x27, 0xC8};
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0) {
+    success = "Delivery Success :)";
+  }
+  else {
+    success = "Delivery Fail :(";
+  }
+}
+
+
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&incomingMessage, incomingData, sizeof(incomingMessage));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  chReceiveFlag = incomingMessage.flag;
+
+}
+
+
+
 
 /* There are several ways to create your ADS1115_WE object:
    ADS1115_WE adc = ADS1115_WE()             -> uses Wire / I2C Address = 0x48
@@ -39,7 +94,33 @@ void setup() {
   pinMode(23, INPUT_PULLUP);
   attachInterrupt(23, ISR, FALLING);
 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
   ADConverterInit();
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+
+    // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  esp_now_register_recv_cb(OnDataRecv);
 
 }
 
@@ -65,11 +146,23 @@ void setup() {
         //midVal=midVal/7704;
         midVal = midVal / 100;
         Serial.println(midVal);
+        message2send.accumulatedValue100times = midVal;
+        message2send.counter = counter;
         Serial.println(" ");
         midVal = 0;
         counter = 0;
         Serial.println(timediff);
         timediff = now;
+          // Send message via ESP-NOW
+        esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &message2send, sizeof(message2send));
+        
+        if (result == ESP_OK){
+          Serial.println("Ne treba cuvati podatke");
+        }
+        else{
+          Serial.println("Treba napisati neko logovanje");
+        }
+        
         /*
           timediff=millis()-timediff;
           Serial.println(timediff);
